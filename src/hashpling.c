1a6beb5c6a2cd85cea698a5a4bd9c2e0c8c8dec5
Mohd Kholid <mrharmonies@live.com>
2018-07-27 10:21:54 +0800

    Fix Buffer-overflown

--- src/hashpling.c
+++ src/hashpling.c
@@ -31,44 +31,31 @@ int main(int argc, char **argv) {
 	}
 
 	if (!strcmp(argv[1],"-v")) {
-		printf("Hashpling v%s. Made with love by Muhamad Khalid Muzappa Yakub. For license info, execute using '-l' option\n",HPVERSION);
+		printf("Hashpling v%s.\n\nMade with love by Muhamad Khalid Muzappa Yakub. For license info, execute using '-l' option\n",HPVERSION);
 		return 0;
 	}
 
 	if (!strcmp(argv[1],"-l")) {
 		printf("Copyright (C) 2018 Mohd Kholid Yaacob (mrharmonies.blogspot.com)\n\n");
-
-		printf("This source is free software; you can redistribute it and/or modify it under\n");
-		printf("the terms of the GNU General Public License as published by the Free\n");
-		printf("Software Foundation; either version 2 of the License, or (at your option)\n");
-		printf("any later version.\n\n");
-
-		printf("This code is distributed in the hope that it will be useful, but WITHOUT ANY\n");
-		printf("WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS\n");
-		printf("FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more\n");
-		printf("details.\n\n");
-
-		printf("A copy of the GNU General Public License is available on the World Wide Web\n");
-		printf("at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing\n");
-		printf("to the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,\n");
-		printf("Boston, MA 02110-1335, USA.\n");
+		printf("This source is free software; you can redistribute it and/or modify it under\nthe terms of the GNU General Public License as published by the Free\nSoftware Foundation; either version 2 of the License, or (at your option)\nany later version.\n\n");
+		printf("This code is distributed in the hope that it will be useful, but WITHOUT ANY\nWARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS\nFOR A PARTICULAR PURPOSE.  See the GNU General Public License for more\ndetails.\n\n");
+		printf("A copy of the GNU General Public License is available on the World Wide Web\nat <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing\nto the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,\nBoston, MA 02110-1335, USA.\n");
 		return 0;
 	}
 
 	if (!strcmp(argv[1],"-h")) {
-		printf("Hashpling v%s.\n",HPVERSION);
-		printf("To use, run \"%s <filename> [arg1 arg2 ..]\" in command line.\n",argv[0]);
-		printf("Shebang format is #!(interpreter) [interpreter arguments].\nThe (interpreter) can be absolute path to an interpreter or just an interpreter name if its already in PATH variable.\n");
+		printf("Hashpling v%s.\n\n",HPVERSION);
+		printf("To use, run \"%s <filename> [arg1 arg2 ..]\" in command line.\n\n",argv[0]);
+		printf("Shebang format is #!(interpreter) [interpreter arguments]. The (interpreter)\ncan be absolute path to an interpreter or just an interpreter name if its\nalready in PATH variable.\n");
 		return 0;
 	}
 
-	char catopt[BUFFER], optionstr[BUFFER];
+	char optionstr[BUFFER];
 	int i;
 
 	if(argc>1) {
 		for (i=2; i<argc; i++) {
-      			snprintf(catopt,BUFFER," \"%s\"",argv[i]);
-      			strncat(optionstr,catopt,BUFFER);
+			snprintf(optionstr,BUFFER,"%s \"%s\"",optionstr,argv[i]);
 		}
 	}
 
@@ -80,9 +67,9 @@ int main(int argc, char **argv) {
 
 	char s[BUFFER];
 	int l=0;
-	
+
 	fgets(s,BUFFER,f);
-	
+
 	while (1) {
 		if (s[l]=='\r'||s[l]=='\n'||s[l]==EOF) {
 			s[l]='\0';
@@ -97,7 +84,7 @@ int main(int argc, char **argv) {
 	char cmd[BUFFER];
 
 	if(s[0]=='#' && s[1]=='!') {
-    snprintf(launcher,BUFFER,"%s",s+2);
+		snprintf(launcher,BUFFER,"%s",s+2);
 		snprintf(cmd,BUFFER,"%s \"%s\"%s",launcher,argv[1],optionstr);
 		return system(cmd);
 	} else {
